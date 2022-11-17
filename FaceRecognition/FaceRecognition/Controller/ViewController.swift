67b6a3786658c96d929581af54165106e96542ee
busrademirbasozkan <busrademirbasozkan@gmail.com>
2022-11-17 23:20:33 +0300

    FaceId eklendi.

--- FaceRecognition/FaceRecognition/Controller/ViewController.swift
+++ FaceRecognition/FaceRecognition/Controller/ViewController.swift
@@ -6,14 +6,38 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel : UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    
+    @IBAction func signinbutton (sender:AnyObject){
+        
+        let authContext = LAContext() // doğrulama işlemleri için kullandığımız obje
+        var error : NSError?
+        //biometric (faceid,touchid) ile belirleme
+        if authContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error){
+            
+            authContext.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "Is it you") { (success, error) in
+                if success == true {
+                    DispatchQueue.main.async {
+                        self.performSegue(withIdentifier: "toSVC", sender: nil)
+                    }
+                }else{
+                    DispatchQueue.main.async{
+                        self.myLabel.text = "Error!"
+                    }
+                }
+            }
+        }
+    }
 }
 
